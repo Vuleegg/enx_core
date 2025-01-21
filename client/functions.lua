c2833e98b1ad474ee2684e7b58837c5d9c14c123
PRODUCT OWNER @ ENX <107327640+Vuleegg@users.noreply.github.com>
2025-01-21 21:28:39 +0100

    .

--- client/functions.lua
+++ client/functions.lua
@@ -1,3 +1,4 @@
+local Intervals = {}
 
 ENX.Thread = function(func)
     if type(func) ~= "function" then
@@ -12,4 +13,22 @@ ENX.Thread = function(func)
             print("^1[ENX.Thread ERROR]^7: " .. err)
         end
     end)
+end
+
+ENX.SetInterval = function(id, msec, callback, onclear)
+    if not Intervals[id] and msec then
+        Intervals[id] = msec
+        CreateThread(function()
+            repeat
+                local interval = Intervals[id]
+                Wait(interval)
+                callback(interval)
+            until interval == -1 and (onclear and onclear() or true)
+            Intervals[id] = nil
+        end)
+    elseif msec then Intervals[id] = msec end
+end
+
+ENX.ClearInterval = function(id)
+    if Intervals[id] then Intervals[id] = -1 end
 end
