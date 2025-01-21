3e5be7d2d0ce28d958c761b2a09ea21b6c50ced2
PRODUCT OWNER @ ENX <107327640+Vuleegg@users.noreply.github.com>
2025-01-21 09:30:48 +0100

    .

--- client/events.lua
+++ client/events.lua
@@ -2,37 +2,5 @@ require 'config.main'
 require 'client.defaults'
 
 RegisterNetEvent("esx:playerLoaded", function(xPlayer)
-    ENX.PlayerData = xPlayer
-
-    if not Multichar then
-
-    end
-
-    while not DoesEntityExist(cache.ped) do
-        Wait(20)
-    end
-
-    ENX.PlayerLoaded = true
-
-    local timer = GetGameTimer()
-    while not HaveAllStreamingRequestsCompleted(cache.ped) and (GetGameTimer() - timer) < 2000 do
-        Wait(0)
-    end
-
     extensions:Load()
-
-    ClearPedTasksImmediately(cache.ped)
-
-    if not Config.Multichar then
-        Core.FreezePlayer(false)
-    end
-
-    if IsScreenFadedOut() then
-        DoScreenFadeIn(500)
-    end
-
-    Actions:Init()
-    StartPointsLoop()
-    StartServerSyncLoops()
-    NetworkSetLocalPlayerSyncLookAt(true)
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -22,6 +22,7 @@ client_scripts {
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
+  'modules/init.lua',
   'server/identifiers.lua',
   'server/player_groups.lua',
   'server/functions.lua',
