f7518a0da437ac77f8ba7bbf1fbd5e5738a08d54
PRODUCT OWNER @ ENX <107327640+Vuleegg@users.noreply.github.com>
2025-02-13 18:18:15 +0100

    added 2 more new functions

--- server/functions.lua
+++ server/functions.lua
@@ -4,12 +4,22 @@ GlobalState.MaximumSlots = GetConvarInt('sv_maxclients', 48)
 
 ---@param source number The players source ID.
 ---@return table|nil The players last known location.
----@field extensions.disableNPCDrops boolean Whether to disable NPC loot drops.
-local function getCoords(source)
+getCoords = function(source)
     if not source or source == 0 then return nil end
     
     local player = server.enx.Cache.getUser(tonumber(source))
-    return player and player.charinfo.last_location or nil
+    return player.charinfo.last_location or nil
 end
 
 exports('getCoords', getCoords)
+
+---@param source number The players source ID.
+---@return string|nil The players full name.
+getCharName = function(source)
+    if not source or source == 0 then return nil end
+    
+    local player = server.enx.Cache.getUser(tonumber(source))
+    return player.charinfo.first_name .. " " .. player.charinfo.last_name or nil
+end
+
+exports('getCharName', getCharName)
