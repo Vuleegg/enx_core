291f8f468f8b744ebf46197929d986bb3738bc8c
PRODUCT OWNER @ ENX <107327640+Vuleegg@users.noreply.github.com>
2025-02-13 17:59:39 +0100

    major changes into functions.lua for clietn and server

--- client/functions.lua
+++ client/functions.lua
@@ -1,6 +1,6 @@
 local config = require 'config.main'
 
-enx.SendAlert = function(data)
+SendAlert = function(data)
   if not data then return print("Data information is missing") end 
     exports['enx-notify']:SendAlert({
         title = data.title,
@@ -11,45 +11,45 @@ enx.SendAlert = function(data)
     })
 end
 
-exports('SendAlert', enx.SendAlert)
+exports('SendAlert', SendAlert)
 
 RegisterNetEvent("enx_core:client:SendAlert") 
  AddEventHandler("enx_core:client:SendAlert", function(data)
-    enx.SendAlert(data)
+    SendAlert(data)
 end)
 
-enx.SetVehicleCustoms = function(data)
+SetVehicleCustoms = function(data)
     if not data then return print("Data information is missing") end 
     
 end
 
-exports('SetVehicleCustoms', enx.SetVehicleCustoms)
+exports('SetVehicleCustoms', SetVehicleCustoms)
 
-enx.GetVehicleCustoms = function(entity)
+GetVehicleCustoms = function(entity)
     if not entity then return print("Entity is missing") end 
     
 end
 
-exports('GetVehicleCustoms', enx.GetVehicleCustoms)
+exports('GetVehicleCustoms', GetVehicleCustoms)
 
-enx.Trim = function(value)
+Trim = function(value)
     if not value then return nil end
     return (string.gsub(value, '^%s*(.-)%s*$', '%1'))
 end
 
-exports('Trim', enx.Trim)
+exports('Trim', Trim)
 
-enx.getPlate(vehicle)
+getPlate(vehicle)
     if vehicle == 0 then return end
-    return enx.Trim(GetVehicleNumberPlateText(vehicle))
+    return Trim(GetVehicleNumberPlateText(vehicle))
 end
 
-exports('getPlate', enx.getPlate)
+exports('getPlate', getPlate)
 
 RegisterNetEvent('enx:Teleport', function()
     local blipMarker = GetFirstBlipInfoId(8)
     if not DoesBlipExist(blipMarker) then
-        enx.SendAlert({
+        SendAlert({
             description = L('error.no_waypoint'), 
             type = 'error', 
             title = L('main_title') 
@@ -116,14 +116,14 @@ RegisterNetEvent('enx:Teleport', function()
 
     if not found then
         SetPedCoordsKeepVehicle(cache.ped, oldCoords.x, oldCoords.y, oldCoords.z - 1.0)
-        enx.SendAlert({
+        SendAlert({
             description = L('error.tp_error'), 
             type = 'error', 
             title = L('main_title') 
         })
     else
         SetPedCoordsKeepVehicle(cache.ped, x, y, groundZ)
-        enx.SendAlert({
+        SendAlert({
             description = L('success.teleported_waypoint'), 
             type = 'success', 
             title = L('main_title') 
--- server/functions.lua
+++ server/functions.lua
@@ -1,1 +1,15 @@
+local server = require 'server.main'
+
 GlobalState.MaximumSlots = GetConvarInt('sv_maxclients', 48)
+
+---@param source number The players source ID.
+---@return table|nil The players last known location.
+---@field extensions.disableNPCDrops boolean Whether to disable NPC loot drops.
+local function getCoords(source)
+    if not source or source == 0 then return nil end
+    
+    local player = server.enx.Cache.getUser(tonumber(source))
+    return player and player.charinfo.last_location or nil
+end
+
+exports('getCoords', getCoords)
