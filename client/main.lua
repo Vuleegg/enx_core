009faf6004d535e77e779e93ce6c369f6db17dcc
PRODUCT OWNER @ ENX <107327640+Vuleegg@users.noreply.github.com>
2025-02-13 18:33:26 +0100

    added getplayerdata function and added announcination how properly to user these functions

--- client/main.lua
+++ client/main.lua
@@ -3,8 +3,10 @@ Cache = {}
 
 exports("loadCore", enx)
 
+---@param object string The key under which the player metadata is stored.
+---@param value any The value to be assigned (can be a table or any other type).
 enx.Cache.setPlayerMeta = function(object, value)
-    if type(object) ~= "string" or object == "" then return  end
+    if type(object) ~= "string" or object == "" then return end
     if value == nil then return end
 
     if type(value) == "table" then
@@ -22,17 +24,26 @@ AddStateBagChangeHandler('PlayerReady', ('player:%s'):format(cache.serverId), fu
     enx.Cache.setPlayerMeta("PlayerReady", value)
 end)
 
-enx.onPlayerReady = function(func)
+---@param func function The function to execute when the player is ready.
+enx.Cache.onPlayerReady = function(func)
     if type(func) ~= "function" then return end
     if not enx.Cache["PlayerReady"] then return end
 
     func()
 end
 
-exports('onPlayerReady', enx.onPlayerReady)
+exports('onPlayerReady', enx.Cache.onPlayerReady)
 
+---@param playerId number The players server ID.
+---@param Player table The players data containing character info.
 RegisterNetEvent("enx_core:client:onPlayerReady", function(playerId, Player)
     enx.Cache.setPlayerMeta("charinfo", Player.charinfo)
     enx.Cache.setPlayerMeta("userId", playerId)
 end)
 
+---@return table|nil The character information stored in the cache.
+enx.Cache.getPlayerData = function()    
+    return enx.Cache['charinfo']
+end
+
+exports('getPlayerData', enx.Cache.getPlayerData)
