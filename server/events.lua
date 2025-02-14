eeed674cb53c97edfec5e625e8c90d97a15ede8a
PRODUCT OWNER @ ENX <107327640+Vuleegg@users.noreply.github.com>
2025-02-14 13:16:42 +0100

    added aduty and hudden duty logic

--- server/events.lua
+++ server/events.lua
@@ -1,4 +1,6 @@
 local server = require 'server.main' 
+local config = require 'config.main'
+local func = require 'server.functions'
 
 AddEventHandler("playerConnecting", function(name, setReason, deferrals)
     TriggerEvent("enx:onPlayerJoinQueue")
@@ -8,6 +10,13 @@ end)
 RegisterNetEvent('enx_core:server:onPlayerReady', function()
     Player(source).state:set('PlayerReady', true, true)
     local player = enx.Cache.getUser(source)
+    enx.Cache.setGroup(source, player.charinfo.group)
+    if config.AdminGroups[player.charinfo.grou].hiddenDuty then
+        func.toggleHiddenDuty(source, true)
+    else
+        func.toggleHiddenDuty(source, false)
+    end
+    func.toggleDuty(source, false)
     TriggerClientEvent("enx_core:client:onPlayerReady", source, player.userId, player)
     if player.charinfo.isNew then 
         TriggerClientEvent("enx_core:client:openRegisterMenu", source)
